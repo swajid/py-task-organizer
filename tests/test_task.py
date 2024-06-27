29acf57ae8ec8e414e1b13286878f848430a13c3
swajid <imsanaw@gmail.com>
2024-06-27 16:48:21 -0400

    -

---
+++ tests/test_task.py
@@ -0,0 +1,17 @@
+import unittest
+from task import Task
+
+
+class test_task(unittest.TestCase):
+    def setUp(self):
+        self.task = Task("d", "get plates", 1000)
+
+    def test_initial_state(self):
+        self.assertFalse(self.task.is_completed)
+        self.assertEqual(self.task.task
+                         , "get plates")
+        self.assertEqual(self.task.status, "d")
+        self.assertEqual(self.task.priority, 1000)
+
+if __name__ == "__main__":
+    unittest.main()
